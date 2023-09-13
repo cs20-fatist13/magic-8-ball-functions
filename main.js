ce66929e84bccbd217101e38f8d5fe574e324e11
Basaam Anwar <109045924+fatist13@users.noreply.github.com>
2023-09-13 12:44:16 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 // Program Title
 document.getElementById("btn").innerHTML.addEventListener("click", btnCLicked);
 
-function btnCLicked() {}
-
+function btnCLicked() {
+  getResponse();
+}
 
 function getResponse() {
   //let response = document.getElementById("response").value;
@@ -11,12 +12,28 @@ function getResponse() {
   if (question == "Does a magic 8 ball actually work?") {
     document.getElementById("response").innerHTML = "How dare you doubt me!";
   } else if (question == "Is Javascript awesome?") {
-      document.getElementById("response").innerHTML = "Of course!";
+    document.getElementById("response").innerHTML = "Of course!";
   } else {
-      
+    randomResponse();
   }
 }
 
 function randomResponse() {
-    
+  let randResponse = Math.random();
+
+  if (randResponse < 0.25) {
+    document.getElementById("response").innerHTML = "Without a Doubt.";
+  } else if (randResponse < 0.5) {
+    document.getElementById("response").innerHTML = "As I see it, yes.";
+  } else if (randResponse < 0.75) {
+    document.getElementById("response").innerHTML =
+      "Concentrate and ask again.";
+  } else if (randResponse < 1) {
+    document.getElementById("response").innerHTML = "Don't count on it.";
+  } else if (randResponse < 1.25) {
+    document.getElementById("response").innerHTML = "Outlook not so good.";
+  } else {
+    document.getElementById("response").innerHTML = "Please ask a question.";
+  }
+  return randResponse;
 }
