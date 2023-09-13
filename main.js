78edd8fb6105f675b2cc9b3aced75e4af9475005
Basaam Anwar <109045924+fatist13@users.noreply.github.com>
2023-09-13 17:25:46 -0600

    k

--- main.js
+++ main.js
@@ -9,9 +9,9 @@ function getResponse() {
   //let response = document.getElementById("response").value;
   let question = document.getElementById("question").value;
 
-  if (question == "Does a magic 8 ball actually work?") {
+  if (question === "Does a magic 8 ball actually work?") {
     document.getElementById("response").innerHTML = "How dare you doubt me!";
-  } else if (question == "Is Javascript awesome?") {
+  } else if (question === "Is Javascript awesome?") {
     document.getElementById("response").innerHTML = "Of course!";
   } else {
     randomResponse();
