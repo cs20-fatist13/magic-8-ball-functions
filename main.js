aa8a48d2aea829428ec088c08c04bca55d4fc124
Basaam Anwar <109045924+fatist13@users.noreply.github.com>
2023-09-13 12:38:14 -0600

    gcgh

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
 // Program Title
+document.getElementById("btn").innerHTML.addEventListener("click", btnCLicked);
+
+function btnCLicked() {}
+
+
+function getResponse() {
+  //let response = document.getElementById("response").value;
+  let question = document.getElementById("question").value;
+
+  if (question == "Does a magic 8 ball actually work?") {
+    document.getElementById("response").innerHTML = "How dare you doubt me!";
+  } else if (question == "Is Javascript awesome?") {
+      document.getElementById("response").innerHTML = "Of course!";
+  } else {
+      
+  }
+}
+
+function randomResponse() {
+    
+}
