383aafdba6959e17d30a2b8520c2be0b9a5bcef4
Basaam Anwar <109045924+fatist13@users.noreply.github.com>
2023-09-13 17:28:51 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,36 +1,37 @@
-// Program Title
-document.getElementById("btn").innerHTML.addEventListener("click", btnClicked);
+// Magic 8 Ball
+document.getElementById("btn").addEventListener("click", btnClicked);
 
 function btnClicked() {
   getResponse();
 }
 
 function getResponse() {
-  //let response = document.getElementById("response").value;
   let question = document.getElementById("question").value;
+  let response = document.getElementById("response");
 
   if (question === "Does a magic 8 ball actually work?") {
-    document.getElementById("response").innerHTML = "How dare you doubt me!";
+    response.innerHTML = "How dare you doubt me!";
   } else if (question === "Is Javascript awesome?") {
-    document.getElementById("response").innerHTML = "Of course!";
+    response.innerHTML = "Of course!";
   } else {
-    randomResponse();
+    randomResponse(response);
   }
 }
 
-function randomResponse() {
+function randomResponse(response) {
   let randResponse = Math.random();
 
   if (randResponse < 0.25) {
-    document.getElementById("response").innerHTML = "Without a Doubt.";
+    response.innerHTML = "Without a Doubt.";
   } else if (randResponse < 0.5) {
-    document.getElementById("response").innerHTML = "As I see it, yes.";
+    response.innerHTML = "As I see it, yes.";
   } else if (randResponse < 0.75) {
-    document.getElementById("response").innerHTML =
-      "Concentrate and ask again.";
+    response.innerHTML = "Concentrate and ask again.";
   } else if (randResponse < 1) {
-    document.getElementById("response").innerHTML = "Don't count on it.";
+    response.innerHTML = "Don't count on it.";
   } else if (randResponse < 1.25) {
-    document.getElementById("response").innerHTML = "Outlook not so good.";
+    response.innerHTML = "Outlook not so good.";
+  } else {
+    response.innerHTML = "Please ask a question.";
   }
 }
